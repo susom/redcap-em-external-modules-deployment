ef44ad725fbab0a24541a154783973e551e97153
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-06-17 13:07:21 -0700

    only update record for new or default records.

--- pages/webhook.php
+++ pages/webhook.php
@@ -12,6 +12,7 @@ try {
     $input = trim(file_get_contents('php://input'));
     $data = json_decode($input, true);
 
+    // test commit.
     if (!empty($data)) {
         $module->emLog($data['repository']['name']);
 //        $payload = json_decode($data, true);
